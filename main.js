8d29bd884115854dfd301262f83681c83a031522
MariaIsMe <ayen.maramag@gmail.com>
2023-08-25 15:44:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,24 @@ setTimeout(function(){
 	location.reload();  
 }, 3000);*/
 
+/*Fin doesn't work '*/
 setTimeout(()=>{
   // uses HTML5 history API to manipulate the location bar
   history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
 }, 5); // 5 millisecond timeout in this case
+
+/*https://www.linkedin.com/pulse/how-refresh-page-only-once-javascript-mert-kadir-gursoy/*/
+function reloadPage() {
+// The last "domLoading" Time //
+var currentDocumentTimestamp =
+new Date(performance.timing.domLoading).getTime();
+// Current Time //
+var now = Date.now();
+var fiveSec = 5 * 1000;
+var plusFiveSec = currentDocumentTimestamp + fiveSec;
+if (now > plusFiveSec) {
+location.reload();
+} else {}
+}
+
+reloadPage();
